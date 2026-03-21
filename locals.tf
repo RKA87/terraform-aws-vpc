82bc60792a4cf5784ca7ddd64d7eb1a62c10dc85
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-03-21 17:39:20 -0500

    configuring AWS VPC, IGW, Subnets with AZ's  using terraform

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
     Project     = var.project
     Deployment  = "terraform"
   }
-  
+
   vpc_final_tags = merge(local.common_tags, var.vpc_tags,
     {
       Name = "${var.project}-${var.environment}-vpc"
@@ -17,10 +17,10 @@ locals {
     }
   )
 
-  public_subnet_final_tags = merge(local.common_tags, var.public_subnet_tags, var.pub_subnet_tags)
+  public_subnet_final_tags = merge(local.common_tags, var.public_subnet_tags)
 
-  private_subnet_final_tags = merge(local.common_tags, var.private_subnet_tags, var.priv_subnet_tags)
+  private_subnet_final_tags = merge(local.common_tags, var.private_subnet_tags)
 
-  database_subnet_final_tags = merge(local.common_tags, var.database_subnet_tags, var.db_subnet_tags)
+  database_subnet_final_tags = merge(local.common_tags, var.database_subnet_tags)
 
 }
